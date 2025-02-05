332847ca25a628475e9d68dc75657f43fe2a6876
Alvin Arrazy <alvinarrazy29@gmail.com>
2025-02-05 11:38:13 +0700

    feat: added /

--- delta-indie/server/router/index.ts
+++ delta-indie/server/router/index.ts
@@ -4,5 +4,8 @@ import authRoutes from './auth';
 const router = express.Router();
 
 router.use('/auth', authRoutes);
+router.get('/', async function (_, res) {
+  res.status(200).json({ message: 'deployed successfully' });
+});
 
 export default router;
