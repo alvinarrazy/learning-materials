08fbef7b91ebbf948ad7d646de7ff08cefdc5a32
Alvin Arrazy <alvinarrazy29@gmail.com>
2025-02-11 20:27:34 +0700

    fix: re-added check password

--- delta-indie/server/service/auth/index.ts
+++ delta-indie/server/service/auth/index.ts
@@ -5,12 +5,12 @@ import { createNewUser, getUserByUsername } from '../../repository/user';
 const SECRET = 'secret';
 
 export async function generateToken(username: string, password: string) {
-  // const user = await getUserByUsername(username);
+  const user = await getUserByUsername(username);
 
-  // const isMatch = await bcrypt.compare(password, user.password);
-  // if (!isMatch) {
-  //   throw 401;
-  // }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw 401;
+  }
 
   return jwt.sign({ id: username }, SECRET, { expiresIn: '1h' });
 }
